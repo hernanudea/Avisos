5a27b8d341f6bfbaae51a8c7b581a3f0a3d61411
Hernan Velasquez <hernan@hernan-desktop14.04>
2016-04-11 19:49:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hernan/avisos/AvisosActivity.java
@@ -0,0 +1,13 @@
+package com.example.hernan.avisos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvisosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avisos);
+    }
+}
